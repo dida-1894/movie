8be4036e72ff519664cc8697d833e34d064062e9
Yang Jin <314256325@qq.com>
2018-11-20 16:06:45 +0800

    学习pug模板， 并使用koa-views解析带有变量的pug成为html页面

---
+++ server/temp/normal.js
@@ -0,0 +1,20 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="'viewport" content="'width=device-width, initial-scale=1">
+    <title>Koa server HTML</title>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1>Hi movie</h1>
+                    <p>this is a movie site</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </head>
+    </html>
+`
